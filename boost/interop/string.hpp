ba366a4b69b03e31e1e671a0f2ab670cb7f1c5fe
Beman <bdawes@acm.org>
2011-07-27 10:49:21 -0400

    Initial commit

---
+++ boost/interop/string.hpp
@@ -0,0 +1,9 @@
+//  boost/interop/string.hpp  ----------------------------------------------------------//
+
+//  Copyright Beman Dawes 2011
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/interop/string_0x.hpp>
+
