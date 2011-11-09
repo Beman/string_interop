c65cee7aeed8d96abcf62a9db07ede74edeb9762
Beman <bdawes@acm.org>
2011-11-09 08:08:09 -0500

    Fix include of renamed header

--- libs/interop/src/codepage_437.cpp
+++ libs/interop/src/codepage_437.cpp
@@ -5,7 +5,7 @@
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/interop/conversion_iterator_adapters.hpp>
+#include <boost/interop/iterator_adapter.hpp>
 #include <boost/static_assert.hpp>
 
 namespace boost
