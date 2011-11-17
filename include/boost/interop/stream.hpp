ed6c9cde92260f0c120de1cd42f9ec8adb8471d0
Beman <bdawes@acm.org>
2011-11-17 09:10:21 -0500

    stream_test container overloads now passing for both VC++10 and g++ 4.5, 4.6

--- include/boost/interop/stream.hpp
+++ include/boost/interop/stream.hpp
@@ -13,8 +13,38 @@
 #include <boost/interop/detail/is_iterator.hpp>
 #include <boost/utility/enable_if.hpp>
 #include <boost/mpl/logical.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/type_traits/remove_const.hpp>
 #include <ostream>
 #include <iterator>
+
+
+
+namespace boost
+{
+  namespace detail
+  {
+    template <class T, bool Pred> struct iterator_value_helper;
+    template <class T> struct iterator_value_helper<T, true>
+    { 
+      typedef typename std::iterator_traits<T>::value_type type;
+    };
+    struct not_an_iterator;
+    template <class T> struct iterator_value_helper<T, false>
+    { 
+      typedef not_an_iterator type;
+    };
+
+    template <class T>
+    struct iterator_value
+    {
+      typedef
+        typename iterator_value_helper<T, is_iterator<T>::value >::type type;
+    };
+
+  } // namespace detail
+} // namespace boost
+
 
 //--------------------------------------------------------------------------------------//
 //                                                                                      //
@@ -28,47 +58,57 @@ namespace std
 {
 
 template <class Ostream, class Ctr>
-//typename boost::enable_if_c<::boost::xop::is_character_container<Ctr>::value
-//    && !::boost::is_same<typename Ctr::value_type, typename Ostream::value_type>::value,
-//  Ostream&>::type
-//typename boost::enable_if<
-//  boost::mpl::and_<boost::xop::is_character_container<Ctr>,
-//                   boost::mpl::not_<
-//    boost::is_same<typename Ctr::value_type, typename Ostream::value_type> > >,
-//  Ostream&>::type
-
-// This did work, except for the second and_ argument being a dummy
-//typename boost::enable_if<
-//  boost::mpl::and_<typename boost::xop::is_character_container<Ctr>,
-//    boost::mpl::true_>,
-//  Ostream&>::type
-
-typename boost::enable_if<typename boost::xop::is_character_container<Ctr>,
+typename boost::enable_if_c<boost::xop::is_character_container<Ctr>::value
+    && !boost::is_same<typename Ctr::value_type, typename Ostream::char_type>::value,
   Ostream&>::type
 operator<<(Ostream& os, const Ctr& ctr)
 {
+  cout << boost::xop::is_character_container<Ctr>::value << ' '
+       << boost::is_same<typename Ctr::value_type, typename Ostream::char_type>::value << ' ';
+  cout << "Ctr\n"; 
   boost::xop::converting_iterator<typename Ctr::const_iterator,
-    typename Ctr::value_type, ::boost::xop::by_range,
+    typename Ctr::value_type, boost::xop::by_range,
     typename Ostream::char_type> itr(ctr.begin(), ctr.end());
   for (; *itr; ++itr)
     os << *itr;
   return os;
 }
 
-template <class Ostream, class InputIterator>
-typename boost::enable_if<typename boost::is_iterator<InputIterator>,
-  Ostream&>::type
-operator<<(Ostream& os, InputIterator begin)
-{
-  cout << "InputIterator stream inserter\n";
-  //converting_iterator<InputIterator,
-  //  typename std::iterator_traits<InputIterator>::value_type, ::boost::xop::by_null,
-  //    typename Ostream::char_type>
-  //  itr(begin);
-  //for (; *itr; ++itr)
-  //  os << *itr;
-  return os;
-}
+//template <class Ostream, class InputIterator>
+//
+////typename boost::enable_if<typename boost::is_iterator<InputIterator>,
+//
+//// worked, but forcing char
+//typename boost::enable_if<
+//  boost::mpl::and_<
+//    typename boost::is_iterator<InputIterator>,
+//    boost::mpl::not_<boost::is_same< typename Ostream::value_type,
+//                                     char
+//                                   > >
+//  >,
+//
+////typename boost::enable_if<
+////  boost::mpl::and_<
+////    typename boost::is_iterator<InputIterator>,
+////    boost::mpl::not_<boost::is_same<
+////      typename Ostream::value_type,
+////      typename boost::detail::iterator_value<InputIterator>::type
+////          > // is_same
+////        > // not
+////      > // and
+////    ,
+//Ostream& >::type // enable_if
+//operator<<(Ostream& os, InputIterator begin)
+//{
+//  cout << "InputIterator stream inserter\n";
+//  boost::xop::converting_iterator<InputIterator,
+//    typename std::iterator_traits<InputIterator>::value_type, ::boost::xop::by_null,
+//      typename Ostream::char_type>
+//    itr(begin);
+//  for (; *itr; ++itr)
+//    os << *itr;
+//  return os;
+//}
 
 }  // namespace std
 
--- test/stream_test.cpp
+++ test/stream_test.cpp
@@ -36,11 +36,14 @@ namespace
   template <class T>
   void test_insertion(T x)
   {
+    cout << "      test...\n";
     std::stringstream ss;
     std::string result;
     const std::string expected("HelloPipsqueek");
 
-    ss << "Hello" << x << '\n';
+    ss << "Hello";
+    ss << x;
+    ss << '\n';
     ss >> result;
     BOOST_TEST_EQ(result, expected);
   }
@@ -49,16 +52,56 @@ namespace
   {
     cout << "  stream inserter test...\n";
 
+    cout << "    standard libary overloads...\n";
+
+    test_insertion(pipsqueek);
+    test_insertion("Pipsqueek");
     test_insertion(std::string(pipsqueek));
+    test_insertion(xop::string(pipsqueek));
+
+    cout << "    boost container overloads in namespace std...\n";
+
     test_insertion(std::wstring(wpipsqueek));
     test_insertion(boost::u8string(u8pipsqueek));
     test_insertion(boost::u16string(u16pipsqueek));
     test_insertion(boost::u32string(u32pipsqueek));
-    test_insertion(xop::string(pipsqueek));
     test_insertion(xop::wstring(wpipsqueek));
     test_insertion(xop::u8string(u8pipsqueek));
     test_insertion(xop::u16string(u16pipsqueek));
     test_insertion(xop::u32string(u32pipsqueek));
+
+    //std::stringstream ss;
+    //std::string result;
+    //const std::string expected("HelloPipsqueek");
+
+    //cout << "test std::string\n";
+    //ss << "Hello" << std::string(pipsqueek) << '\n';
+    //ss >> result;
+    //cout << "  result is " << result << '\n';
+    //BOOST_TEST_EQ(result, expected);
+    //ss.clear(); result.clear();
+
+    //cout << "test std::wstring\n";
+    //ss << "Hello" << std::wstring(wpipsqueek) << '\n';
+    //ss >> result;
+    //cout << "  result is " << result << '\n';
+    //BOOST_TEST_EQ(result, expected);
+    //ss.clear(); result.clear();
+ 
+    //cout << "test L\"Pipsqueek\"\n";
+    //ss << "Hello" << L"Pipsqueek" << '\n';
+    //ss >> result;
+    //cout << "  result is " << result << '\n';
+    //BOOST_TEST_EQ(result, expected);
+    //ss.clear(); result.clear();
+
+    //cout << "test std::wstring c_str()\n";
+    //ss << "Hello" << std::wstring(wpipsqueek).c_str() << '\n';
+    //ss >> result;
+    //cout << "  result is " << result << '\n';
+    //BOOST_TEST_EQ(result, expected);
+    //ss.clear(); result.clear();
+
   }
 
 }  // unnamed namespace
