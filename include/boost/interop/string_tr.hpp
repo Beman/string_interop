a4a1a66aa70dbcaaa5964303bcb7d5a71fb447ca
Beman <bdawes@acm.org>
2012-03-07 09:52:59 -0500

    Work-in-progress before rename

--- include/boost/interop/string_tr.hpp
+++ include/boost/interop/string_tr.hpp
@@ -5,6 +5,9 @@
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
 
+#ifndef BOOST_STRING_CONVERSION_HPP
+#define BOOST_STRING_CONVERSION_HPP
+
 //--------------------------------------------------------------------------------------//
 //                                                                                      //
 //           Experimental string extentions for the C++11 standard library              //
@@ -12,37 +15,38 @@
 //--------------------------------------------------------------------------------------//
 
 #include <climits>  // for MB_LEN_MAX
+#include <cwchar>   // for mbstate_t
 #include <algorithm>
 
-//--------------------------------------------------------------------------------------//
-//                                                                                      //
-//                              Header <cuchar> support                                 //
-//                                                                                      //
-//  Very few C++11 standard library implementations support <cuchar>, so for now        //
-//  declare the contents here.                                                          //
-//                                                                                      //
-//--------------------------------------------------------------------------------------//
-
-#define  __STDC_UTF_16__
-#define  __STDC_UTF_32__
-
-namespace std
-{
-  std::size_t mbrtoc16(char16_t* pc16,
-    const char* s, size_t n,
-    std::mbstate_t* ps);
-
-  std::size_t c16rtomb(char* s, char16_t c16,
-    std::mbstate_t* ps);
-
-  std::size_t mbrtoc32(char32_t* pc32, // target
-    const char* s, std::size_t n,      // source
-    std::mbstate_t* ps);
-
-  std::size_t c32rtomb(char* s,        // target
-    std::char32_t c32,                 // source
-    std::mbstate_t* ps);
-}
+////--------------------------------------------------------------------------------------//
+////                                                                                      //
+////                              Header <cuchar> support                                 //
+////                                                                                      //
+////  Very few C++11 standard library implementations support <cuchar>, so for now        //
+////  declare the contents here.                                                          //
+////                                                                                      //
+////--------------------------------------------------------------------------------------//
+//
+//#define  __STDC_UTF_16__
+//#define  __STDC_UTF_32__
+//
+//namespace std
+//{
+//  std::size_t mbrtoc16(char16_t* pc16,
+//    const char* s, size_t n,
+//    std::mbstate_t* ps);
+//
+//  std::size_t c16rtomb(char* s, char16_t c16,
+//    std::mbstate_t* ps);
+//
+//  std::size_t mbrtoc32(char32_t* pc32, // target
+//    const char* s, std::size_t n,      // source
+//    std::mbstate_t* ps);
+//
+//  std::size_t c32rtomb(char* s,        // target
+//    char32_t c32,                      // source
+//    std::mbstate_t* ps);
+//}
 
 //--------------------------------------------------------------------------------------//
 
@@ -174,3 +178,5 @@ namespace std
 
   }
 }
+
+#endif  // BOOST_STRING_CONVERSION_HPP
