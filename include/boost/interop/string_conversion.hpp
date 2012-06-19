9c9a1f3090973e781ea01d902051af45bb8296e0
Beman <bdawes@acm.org>
2012-06-19 15:07:50 -0400

    Final changes before abandoning this conversion approach. A codec approach is becoming more complex that the iterator adapter approach.

--- include/boost/interop/string_conversion.hpp
+++ include/boost/interop/string_conversion.hpp
@@ -1,4 +1,4 @@
-//  boost/interop/string_tr.hpp  -------------------------------------------------------//
+//  boost/interop/string_conversion.hpp  -----------------------------------------------//
 
 //  Copyright Beman Dawes 2012
 
@@ -10,7 +10,7 @@
 
 //--------------------------------------------------------------------------------------//
 //                                                                                      //
-//           Experimental string extentions for the C++11 standard library              //
+//           Experimental string conversions for the C++11 standard library             //
 //                                                                                      //
 //--------------------------------------------------------------------------------------//
 
@@ -19,37 +19,7 @@
 #include <iterator>
 #include <algorithm>
 #include <boost/assert.hpp>
-
-////--------------------------------------------------------------------------------------//
-////                                                                                      //
-////                              Header <cuchar> support                                 //
-////                                                                                      //
-////  Very few C++11 standard library implementations support <cuchar>, so for now        //
-////  declare the contents here.                                                          //
-////                                                                                      //
-////--------------------------------------------------------------------------------------//
-//
-//#define  __STDC_UTF_16__
-//#define  __STDC_UTF_32__
-//
-//namespace std
-//{
-//  std::size_t mbrtoc16(char16_t* pc16,
-//    const char* s, size_t n,
-//    std::mbstate_t* ps);
-//
-//  std::size_t c16rtomb(char* s, char16_t c16,
-//    std::mbstate_t* ps);
-//
-//  std::size_t mbrtoc32(char32_t* pc32, // target
-//    const char* s, std::size_t n,      // source
-//    std::mbstate_t* ps);
-//
-//  std::size_t c32rtomb(char* s,        // target
-//    char32_t c32,                      // source
-//    std::mbstate_t* ps);
-//}
-
+#include <boost/interop/codec.hpp>
 //--------------------------------------------------------------------------------------//
 
 namespace std
